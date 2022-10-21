f9fc067d922184d403f116beeac019ba3f2e1620
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-21 16:54:56 +0700

    add class program n add 4 private int

--- Program.cs
+++ Program.cs
@@ -2,5 +2,14 @@
 
 namespace QuickSort
 {
+    class program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int[] cmp_count = 0; //number of comprasion
+        private int[] mov_count = 0; //number data movements
 
+        //number elements in array
+        private int n;
+    }
 }

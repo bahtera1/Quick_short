e06139d2d310b363bffbaa328e6852757d566589
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-21 17:19:35 +0700

    add void swap

--- Program.cs
+++ Program.cs
@@ -40,6 +40,17 @@ namespace QuickSort
                     arr[i] = Int32.Parse(s1);
                 }
             }
+            
+            //swaps the elements at index x with the elemnts at index y
+            void swap(int x, int y)
+            {
+                int temp;
+
+                temp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = temp;
+            }
+
         }
 
 
